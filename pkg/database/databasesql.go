786f542097874355a892b192b582dfdb4e65c83d
Serguei Vine <vinser@outlook.com>
2022-06-04 17:35:33 +0500

    Setup FTS5 tokenizer

--- pkg/database/databasesql.go
+++ pkg/database/databasesql.go
@@ -20,7 +20,7 @@ CREATE INDEX authots_name_idx ON authors (name);
 CREATE INDEX authots_sort_idx ON authors (sort COLLATE NOCASE);
 
 DROP TABLE IF EXISTS authors_fts;
-CREATE VIRTUAL TABLE authors_fts USING fts5(sort, content='');
+CREATE VIRTUAL TABLE authors_fts USING fts5(sort, content='', tokenize='unicode61 remove_diacritics 2');
 
 DROP TABLE IF EXISTS books;
 CREATE TABLE books (
@@ -46,7 +46,7 @@ CREATE INDEX book_sort_idx ON books (sort COLLATE NOCASE);
 CREATE INDEX book_updated_idx ON books (updated);
 
 DROP TABLE IF EXISTS books_fts;
-CREATE VIRTUAL TABLE books_fts USING fts5(title, content='');
+CREATE VIRTUAL TABLE books_fts USING fts5(title, content='', tokenize='unicode61 remove_diacritics 2');
 
 DROP TABLE IF EXISTS series;
 CREATE TABLE series (
